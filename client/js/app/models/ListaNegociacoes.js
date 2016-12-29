8ac995e937534ace84fc0513c8d396b1db79d98a
Evandro P. Santos <evandro.santos@multilaser.com.br>
2016-12-29 11:36:24 -0200

    Blindando lista de negociacoes

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,13 +1,14 @@
 class ListaNegociacoes {
-	constructor(){
-		this._negociacoes = [];
-	}
-
-	adiciona(negociacao) {
-		this._negociacoes.push(negociacao);
-	}
-
-	get negociacoes() {
-		return this._negociacoes;
-	}
+    
+    constructor() {
+        this._negociacoes = [];
+    }
+    
+    adiciona(negociacao) {
+        this._negociacoes.push(negociacao);
+    }
+    
+    get negociacoes() {        
+        return [].concat(this._negociacoes);
+    }
 }
